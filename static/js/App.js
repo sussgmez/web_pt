93318a0dc9857fe773b60b6fedc69de3aa835884
jesusgdea <jesusgdea@gmail.com>
2025-01-29 23:27:57 -0400

    Optimización v1.0

--- static/js/App.js
+++ static/js/App.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    
+    $("#id_btn_menu").click(function (e) { 
+
+        if($("#id_navbar").is(":visible")) {
+            $("#id_navbar").hide()
+            $("#id_btn_menu").removeClass("active");
+        } 
+        else {
+            $("#id_navbar").css('display', 'flex')
+            $("#id_btn_menu").addClass("active");
+        }
+    });
+
 });
